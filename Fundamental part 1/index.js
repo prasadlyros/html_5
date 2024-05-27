72618c1cc2bcf2ce64299198328806be2dcfbffa
prasadlyros <prasad@lyrostech.com>
2024-05-27 16:11:27 +0530

    files

---
+++ Fundamental part 1/index.js
@@ -0,0 +1,61 @@
+// const calage = birth => 2037-birth;
+// const age2 = calage(1991);
+// console.log(age2);
+
+// const year = (birth, firstname) =>{
+//     const age = 2037-birth;
+//     const retire = 65 - age;
+
+//     return `${firstname} retires in ${retire} years`;
+// }
+
+// console.log(year(1991, `prasad`));
+// console.log(year(1980, `hari`));
+///////////
+// function hello(number1, number2){
+//     const sum = number1+number2;
+//     const name = `${number1} is older than ${number2}`;
+//     console.log(name);
+//     console.log(sum);
+// }
+// hello(1234567890,1234567892);
+// hello(`prasad`, `hari`);
+
+// const re =(birth, name)=>{
+//     const age = 2024-birth;
+//     const rea = 65-age;
+
+//     return `${name} is still having ${rea} years for retirement`;
+// }
+// console.log(re(1999, "Prasad"));
+
+// const input = `19`;
+// console.log(input, typeof input);
+// console.log(Number(input), input);
+// console.log(Number(input)+18);
+
+// == vs ===
+// const age = `12`;
+// if (age===12){
+//     console.log(`true`);
+// }
+// else{
+//     console.log(`false`);
+// }
+//conditional ternary operator
+// const age = 23;
+// age >= 18 ? console.log("drive car"): console.log(`not eligible`);
+//Assignment operator
+// let x = 10;
+// x+=10;
+// console.log(x);
+// // prompt
+// const fav = Number(prompt(`fav number`));
+// console.log(fav);
+// console.log(typeof fav);
+const ar = new Array(1,2,3,4,5,6);
+console.log(ar.length - 1);
+
+const fn = `hari`;
+const phani = [fn, 2037-1245, `teacher`, ar];
+console.log(phani[3]);
